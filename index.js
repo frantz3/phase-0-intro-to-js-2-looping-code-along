3c418df9de0da52f4949905436ad2b3176179bde
frantzmoseau <frantzmoseau35@gmail.com>
2022-07-05 17:30:31 -0400

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solutions in this file
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bowl`);
+      
+    }
+
+    return gifts;
+}
+
+wrapGifts(gifts);
+
+const names = []
+    
+function writeCards(names, event) {
+    const messages = []
+    for (let i = 0; i < names.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+        messages.push(message);
+    }
+    return messages;
+}
+
+console.log(writeCards([names], `birthday`));
+
+function countDown() {
+     for(let i = 10; i >= 0; i--) {
+        if(i==0) {console.log(0);} 
+        else {console.log(i);}
+     }
+        
+}
+
+
